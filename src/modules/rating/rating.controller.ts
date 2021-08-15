90f00b0aaeb5f009e14c969d5869173ff4d7a1a0
Boris Sulicenko <b.sulicenko@gmail.com>
2021-08-15 19:33:35 +0200

    #8

--- src/modules/rating/rating.controller.ts
+++ src/modules/rating/rating.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { DeepPartial } from 'typeorm';
 import { PostService } from '../post/post.service';
 import { Rating } from './rating.entity';
@@ -25,7 +25,7 @@ export class RatingController {
         return this.ratingService.create(post, payload)
     }
 
-    @Delete()
+    @Patch()
     async remove(@Body() payload: DeepPartial<Rating>) {
         const post = await this.postService.findById(payload.post.id)
         const userId = payload.userId
